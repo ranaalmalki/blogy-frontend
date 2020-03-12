5829550d699be6fe99e1ad61f07e1333732e1898
ranaalmalki <ranaalmalki12@gmail.com>
2020-03-12 11:11:46 +0300

    Delete Article by ID using API

--- src/articles/components/article.js
+++ src/articles/components/article.js
@@ -1,14 +1,21 @@
-import React from 'react';
-class Article extends React.Component{
-render(){
-    return(
-        <div className="article">
-{/* title & Content & Author */}
-<h2>{this.props.title}</h2>
-<sub>{this.props.author}</sub>
-<p>{this.props.content}</p>
-        </div>
-    )
-}
+import React from "react";
+class Article extends React.Component {
+  deleteArticle = event => {
+    event.preventDefault();
+    this.props.deleteArticle(this.props.id);
+  };
+  render() {
+    return (
+      <div className="article">
+        {/* title & Content & Author */}
+        <h2>{this.props.title}</h2>
+        <sub>{this.props.author}</sub>
+        <p>{this.props.content}</p>
+        <a href="#" onClick={this.deleteArticle}>
+          Delete
+        </a>
+      </div>
+    );
+  }
 }
 export default Article;
--- src/articles/components/articles.js
+++ src/articles/components/articles.js
@@ -1,37 +1,56 @@
-import React from 'react';
-import Article from './article';
-import { getAllArticles } from '../api';
-class Articles extends React.Component{
-    componentDidMount(){
-                //Make API Call 
-        getAllArticles()
-        .then((response)=>{
-this.props.setArticles(response.data.articles);
-        })
-        .catch((error)=>{
-console.log('API ERROR', error);
-        });
-        /**
-         * INDEX,SHOW, DESTROY , UPDATE,CREATE
-         */
-    }
-    render(){
-        let allArticles =<h4>No Articles! </h4>;
-        if(this.props.articles.length >0){
-        allArticles = this.props.articles.map((article ,index)=>{
-            return <Article title={article.title}
-                            author={article.author}
-                            content={article.content}
-                            key={index}
-            />;
-        });
-    }
-        return(
-            <>
-<h3>All Articles</h3>
-{allArticles}
-</>
+import React from "react";
+import Article from "./article";
+import { getAllArticles, deleteArticleByID } from "../api";
+
+class Articles extends React.Component {
+  componentDidMount() {
+    //Make API Call
+    getAllArticles()
+      .then(response => {
+        this.props.setArticles(response.data.articles);
+    
+      })
+      .catch(error => {
+        console.log("API ERROR", error);
+      });
+  }
+  //Make API Call to Delete an Artcile
+  deleteArticle = id => {
+    console.log("The Article ID to Delete", id);
+    deleteArticleByID(id)
+      .then(response => {
+        console.log(`The Article with the ID ${id} has been deleted.`);
+        const newArticleList =this.props.articles.filter((article)=>{
+            return article._id !== id; 
+         });
+         this.props.setArticles(newArticleList);
+      })
+      .catch(error => {
+        console.log("API ERROR: ", error);
+      });
+  };
+  render() {
+    let allArticles = <h4>No Articles! </h4>;
+    if (this.props.articles.length > 0) {
+      allArticles = this.props.articles.map((article, index) => {
+        return (
+          <Article
+            title={article.title}
+            author={article.author}
+            content={article.content}
+            id={article._id}
+            deleteArticle={this.deleteArticle}
+            key={index}
+          />
         );
+      });
     }
+    return (
+      <>
+        <h3>All Articles</h3>
+        {allArticles}
+      </>
+    );
+  }
 }
 export default Articles;
